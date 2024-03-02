ec958a143cc09b383ee56cd57b918c4e93d7cff0
Raghav Kumar Tonk <86098245+raghavtonk@users.noreply.github.com>
2024-03-02 13:48:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+var line = document.getElementById('line');
+
+line.style.position='absolute';
+line.style.top='50%';
+line.style.left='50%';
+line.style.width='200px';
+line.style.height='2px';
+line.style.backgroundColor ='black';
+
+var angle=0;
+function rotateLine(){
+	angle+=2;
+	line.style.transform = 'translate(-50%, -50%) rotate(' + angle + 'deg)';
+	setTimeout(rotateLine, 20);
+}
+rotateLine();
